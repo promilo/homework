09edd0535476ddbea5a63864c478fc2e241327e1
promilo <milind.shah@mail.utoronto.ca>
2017-11-04 19:18:32 -0400

    added basic test cases

---
+++ index.js
@@ -0,0 +1,17 @@
+const TYPE_MARKUP = {
+  "food" : 0.13,
+  "pharmaceutical": 0.075,
+  "electronic": 0.02,
+  "flat": 0.05
+}
+
+module.exports = {
+  pricePackager: (price, people, type) => {
+  },
+  parseInput: (people) => {
+  },
+  typeMarkup: (type) => {
+
+  }
+
+}
