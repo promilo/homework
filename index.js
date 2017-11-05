87d9a9fab46035b8d4a23d644c2984c886d455ce
promilo <milind.shah@mail.utoronto.ca>
2017-11-04 20:34:47 -0400

    finished calculating persons Markup

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ const MARKUP = {
   "food" : 0.13,
   "pharmaceutical": 0.075,
   "electronic": 0.02,
-  "flat": 0.05
+  "flat": 0.05,
+  "person": 0.012
 }
 
 const pricePackager = {
   calculate: (price, people, type) => {
-    console.log(`Input: ${price}, ${people}, ${type}/n/n`);
+    console.log(`Input: ${price}, ${people}, ${type} /n`);
     console.log(`Base Price: ${price}`);
     let total = price;
     console.log(`Base Price: ${total}`);
-    total = total * MARKUP['flat'];
-    console.log(`Flat Markup: ${total}`)
+    let flatTotal = total * MARKUP['flat'];
+    console.log(`Flat Markup: ${MARKUP["flat"] * 100}% = ${flatTotal}`)
+    total += flatTotal
+    console.log(`Subtotal: ${total}`)
+    let numPeople = pricePackager.parsePeople(people)
+    let peopleMarkup = MARKUP["person"] * numPeople
+    let personsTotal = total * peopleMarkup
+    console.log(`Persons Markup: ${numPeople} * ${MARKUP["person"] * 100}% = ${(peopleMarkup * 100).toFixed(1)}% = ${personsTotal}`)
+    total += personsTotal
+
 
 
 
