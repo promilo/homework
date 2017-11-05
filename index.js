ff7aadab73e3398572654361e2464d873893db64
promilo <milind.shah@mail.utoronto.ca>
2017-11-04 20:26:39 -0400

    finished calculating flat markup

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TYPE_MARKUP = {
+const MARKUP = {
   "food" : 0.13,
   "pharmaceutical": 0.075,
   "electronic": 0.02,
@@ -7,6 +7,13 @@ const TYPE_MARKUP = {
 
 const pricePackager = {
   calculate: (price, people, type) => {
+    console.log(`Input: ${price}, ${people}, ${type}/n/n`);
+    console.log(`Base Price: ${price}`);
+    let total = price;
+    console.log(`Base Price: ${total}`);
+    total = total * MARKUP['flat'];
+    console.log(`Flat Markup: ${total}`)
+
 
 
   },
@@ -14,13 +21,14 @@ const pricePackager = {
     return parseInt(people.charAt(0))
   },
   typeMarkup: (type) => {
-    if (TYPE_MARKUP[type]){
-      return TYPE_MARKUP[type]
+    if (MARKUP[type]){
+      return MARKUP[type]
     }
-  }
+  },
 
 }
 
 console.log(pricePackager.parsePeople("1 person"))
+console.log(pricePackager.calculate(1299.99, "3 people", "food"))
 
 module.exports = pricePackager;
